4027c55c26c713aabf67c0c9b3fdd97b37571ba4
Benjamin Oni <onibenjo@gmail.com>
2022-04-02 15:55:42 +0100

    env server port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 5001;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5001);
+  await app.listen(port);
 }
 bootstrap();
